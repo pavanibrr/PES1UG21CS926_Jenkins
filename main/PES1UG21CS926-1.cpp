56c73d4f76af2574d774d40ed694dc3c92aca39e
Pavani B r <pavanibr@Pavanis-MacBook-Air.local>
2024-03-10 19:16:06 +0530

    Your clear commit message here

---
+++ main/PES1UG21CS926-1.cpp
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    build 'PES1UG21CS926-1'
+                    sh 'g++ main1.cpp -o output'
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                script {
+                    sh './output'
+                }
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying...'
+            }
+        }
+    }
+    
+    post {
+        failure {
+            echo 'Pipeline failed'
+        }
+    }
+}
