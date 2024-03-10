f0c719aeaa9b8ef02956cee3f76260bc79c78b4c
Pavani B r <pavanibr@Pavanis-MacBook-Air.local>
2024-03-10 19:07:14 +0530

    Your clear commit message here

--- main/new.cpp
+++ main/new.cpp
@@ -1,31 +1,20 @@
 pipeline {
-    agent any
+    agent any 
 
     stages {
         stage('Build') {
             steps {
-                script {
-                    // Compile .cpp file using a shell script
-                    sh 'g++ -o myExecutable main/new.cpp'
-                }
+                sh 'g++ -o myExecutable main/new.cpp'
             }
         }
-
         stage('Test') {
             steps {
-                script {
-                    // Print output of .cpp file using a shell script
-                    sh './myExecutable'
-                }
+                sh './myExecutable'
             }
         }
-
         stage('Deploy') {
             steps {
-                script {
-                    // Deployment steps go here (if any)
-                    echo 'Deployment completed successfully'
-                }
+                echo 'Deployment completed successfully'
             }
         }
     }
@@ -33,12 +22,9 @@ pipeline {
     post {
         success {
             echo 'Pipeline succeeded'
-            // Additional actions or notifications for success can be added here
         }
-
         failure {
             echo 'Pipeline failed'
-            // Additional actions or notifications can be added here
         }
     }
 }
